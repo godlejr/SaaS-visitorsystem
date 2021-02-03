cd31a1bb58708707a0bba15b60560308c0500842
hojunghan@hanwha.com <hojunghan@hanwha.com>
2021-02-03 16:58:36 +0900

    출입신청 inout_apply.py, in_out.js 수정

--- visitorsystem/static/js/in_out.js
+++ visitorsystem/static/js/in_out.js
@@ -47,6 +47,14 @@ $(document).ready(function () {
                     reqUrl = reqUrl + `rule/search`;
                     break;
 
+               case 'RULE_SEARCH':
+                    reqUrl = reqUrl + `rule/search`;
+                    break;
+
+                case 'RULE_VALID':
+                    reqUrl = reqUrl + `rule/valid`;
+                    break;
+
           }
           return reqUrl;
      }
@@ -60,54 +68,55 @@ $(document).ready(function () {
 
 
           newTr = `
-        <tr class="hide">
-            <td>
-                    <input type="checkbox" id="checkbox" class="peer">
-            </td>
+         <tr class="hide">
+             <td>
+                     <input type="checkbox" id="checkbox" class="peer">
+             </td>
 
-            <td>
-               <div class="form-group">
-                    <input type="text" class="form-control" style="width:80%; float:left" >
-                </div>
-            </td>
-
-            <td>
+             <td>
                 <div class="form-group">
-                    <input type="text" id="tphone" class="form-control">
-                </div>
-            </td>
+                     <input type="text" class="form-control" id="tvisitor" style="width:80%; float:left" >
+                 </div>
+             </td>
 
-            <td>
-               <div class="form-group" >
-                        <select id="" name="" class="form-control" disabled>
-                            <option selected="selected" value="차량없음">차량없음</option>
-                            <option value="승용차">승용차</option>
-                            <option value="자동차">자동차</option>
-                        </select>
-                </div>
-            </td>
-            <td><input type="text" class="form-control" disabled></td>
-            `
+             <td>
+                 <div class="form-group">
+                     <input type="text" id="tphone"  class="form-control">
+                 </div>
+             </td>
+
+             <td>
+                <div class="form-group" >
+                         <select id="" name="" class="form-control" disabled>
+                             <option selected="selected" value="차량없음">차량없음</option>
+                             <option value="승용차">승용차</option>
+                             <option value="자동차">자동차</option>
+                         </select>
+                 </div>
+             </td>
+             <td><input type="text" class="form-control" disabled></td>
+             `
 
           for (var i = 0; i < dataSet.length; i++) {
                var obj = dataSet[i];
                var type = obj.rule_type;
                var ruleName = obj.rule_name;
-               theadContext = `<th style="">${ruleName}</th>` + theadContext;
+               console.log(ruleName)
+               theadContext = theadContext+`<th style="">${ruleName}</th>`;
 
                if (type == '텍스트') {
                     append = append + `<td>
-                                                <div class="input-group">
-                                                    <input type="text" class="form-control" disabled>
-                                                </div>
-                                   </td>`
+                                                 <div class="input-group">
+                                                     <input type="text" class="form-control" disabled>
+                                                 </div>
+                                    </td>`
                } else if (type == '달력') {
                     append = append + `<td>
-                                            <div class="input-group">
-                                             <div class="input-group-addon bgc-white bd bdwR-0"><i class="ti-calendar"></i></div>
-                                             <input type="text" class="form-control bdc-grey-200 start-date" placeholder="Datepicker" data-provide="datepicker" name ="inout_sdate" id="inout_sdate" disabled>
-                                            </div>
-                                        </td> `
+                                             <div class="input-group">
+                                              <div class="input-group-addon bgc-white bd bdwR-0"><i class="ti-calendar"></i></div>
+                                              <input type="text" class="form-control bdc-grey-200 start-date" placeholder="Datepicker" data-provide="datepicker" name ="inout_sdate" id="inout_sdate" disabled>
+                                             </div>
+                                         </td> `
                } else if (type == '파일') {
                     append = append + `<td><input id="input-b1" name="input-b1" type="file" class="file" data-browse-on-zone-click="true" disabled></td> `
                }
@@ -130,6 +139,10 @@ $(document).ready(function () {
 
           console.log(JSON.stringify(dataSet));
 
+     }
+
+     //규칙검증 컨트롤러
+     function rulevalidController(dataSet){
      }
 
      //event리스너
@@ -167,14 +180,40 @@ $(document).ready(function () {
                     $('#tphone').keydown(function (e) {
 
                          if (e.keyCode == 13) {
-                              $(this).val( $(this).val().replace(/[^0-9]/g, "").replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/,"$1-$2-$3").replace("--", "-") );
-                              var sdate = $('#sdate').split("/");
-                              var temp = sdate[2]; //
-                              var
+                              $(this).val($(this).val().replace(/[^0-9]/g, "").replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/, "$1-$2-$3").replace("--", "-"));
+                              var sdate = $('#inout_sdate').val();
+                              var edate = $('#inout_edate').val();
+                              var date = [];
+                              var year, month, day;
+                              var dataSet = {};
 
-                              a.split("").reverse().
+                              if (!sdate)
+                                   return;
+
+                              sdate = sdate.split('/')
+                              year = sdate[2]; //년
+                              month = sdate[0]; //월
+                              day = sdate[1]; //일
+                              dataSet['sdate'] = year+"-"+month+"-"+day;
+                              date = [];
+
+                              edate = edate.split('/')
+                              year = edate[2]; //년
+                              month = edate[0]; //월
+                              day = edate[1]; //일
+                              dataSet['edate'] = year+"-"+month+"-"+day;
+                              data = [];
+
+                              var userName = $(this).parent().parent().prev().children().children().val();
+                              var userPhone = $(this).val();
+                              dataSet['userName'] = userName;
+                              dataSet['userPhone'] = userPhone;
+                              console.log("테스트")
+                              apiCallPost(urlMake('RULE_VALID'), rulevalidController , dataSet)
+
                          }
                     });
+
                }
                $tableID.find('table').append($clone);
           });
@@ -184,7 +223,7 @@ $(document).ready(function () {
                var tdArray = new Array();
 
                checkbox.each(function (i) {
-                    var tr = checkbox.parent().eq(i);
+                    var tr = checkbox.parent().parent().eq(i);
                     var td = tr.children();
                     setTimeout(function () {
                          tr.remove();
@@ -193,7 +232,6 @@ $(document).ready(function () {
                });
 
           });
-
 
           $('#interviewSearch').click(function (e) {
                var dataSet = {};
--- visitorsystem/views/inout_apply.py
+++ visitorsystem/views/inout_apply.py
@@ -1,17 +1,21 @@
-from flask import Blueprint, render_template, request, redirect, url_for, current_app,jsonify
+from operator import and_
+
+from flask import Blueprint, render_template, request, redirect, url_for, current_app, jsonify
 from flask_login import current_user, login_required
 from visitorsystem.forms import ApplyForm
-from visitorsystem.models import db, Vcapplymaster, Ssctenant, Scrule
+from visitorsystem.models import db, Vcapplymaster, Ssctenant, Scrule, Vcvisituser
+from datetime import datetime
+import time
 
 inout_apply = Blueprint('inout_apply', __name__)
+
+
 
 
 @inout_apply.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-
     if request.method == 'POST':
-        print('호출')
         # print('============================')
         # print(applyform.stringfield.data)
         # print(applyform.textAreaField.data)
@@ -29,28 +33,27 @@ def create():
     if request.method == 'POST':
         applyform = ApplyForm(request.form)
         apply = Vcapplymaster()
-        apply.apply_code ='APPLY-002'
+        apply.apply_code = 'APPLY-002'
         apply.apply_nm = '출입신청 2번'
         apply.interviewr = request.form['interviewer_name']  # 감독자
-        apply.applicant = request.form['applicant_name'] # 신청자
-        apply.phone = request.form['applicant_phone'] # 감독자 휴대폰
-        apply.visit_category = request.form['inout_purpose_type'] # 방문종류
-        apply.biz_no = request.form['inout_biz_no'] # 사업자번호
-        apply.visit_sdate = request.form['inout_sdate'] # 방문시작일
-        apply.visit_edate = request.form['inout_edate'] # 방문종료일
-        apply.visit_purpose = request.form['inout_title'] # 방문목적
-        apply.visit_desc = request.form['inout_purpose_desc']#방문목적 상세기술
+        apply.applicant = request.form['applicant_name']  # 신청자
+        apply.phone = request.form['applicant_phone']  # 감독자 휴대폰
+        apply.visit_category = request.form['inout_purpose_type']  # 방문종류
+        apply.biz_no = request.form['inout_biz_no']  # 사업자번호
+        apply.visit_sdate = request.form['inout_sdate']  # 방문시작일
+        apply.visit_edate = request.form['inout_edate']  # 방문종료일
+        apply.visit_purpose = request.form['inout_title']  # 방문목적
+        apply.visit_desc = request.form['inout_purpose_desc']  # 방문목적 상세기술
         apply.site_id = 'U1'  # 사업장코드
-        apply.site_nm = '울산1공장' # 사업장명
+        apply.site_nm = '울산1공장'  # 사업장명
         # apply.site_id = request.form['inout_location'].data #
         # apply.site_nm = request.form['inout_location_desc'].data
-        apply.login_id = 'admin' #로그인 아이디(세션에서 가져오기)
-        apply.approval_state = '대기' #출입승인 상태 저장
+        apply.login_id = 'admin'  # 로그인 아이디(세션에서 가져오기)
+        apply.approval_state = '대기'  # 출입승인 상태 저장
         db.session.add(apply)
         db.session.commit()
 
     return jsonify({'file_name': 'output'})
-
 
 
 """
@@ -60,55 +63,74 @@ def create():
 003. 표 binding 후 보여주기
 """
 
+
 @inout_apply.route('/rule/search', methods=['POST'])
 def ruleSearch():
-    print('-------------------')
-    print('호출')
-    print('-------------------')
+
     ssctenant = Ssctenant.query.filter_by(event_url=request.host).first()
     tenant_id = ssctenant.tenant_id
 
     lists = []
     for row in db.session.query(Scrule).filter(Scrule.tenant_id == tenant_id).all():
-        scrule = {
+        dict = {
             "rule_name": row.rule_name,
             "rule_type": row.rule_type,
             "rule_duedate": row.rule_duedate,
             "rule_desc": row.rule_desc
         }
-        lists.append(scrule)
+        lists.append(dict)
 
     return jsonify({'msg': lists})
 
-
-@inout_apply.route('/rule/validate', methods=['POST'])
+"""
+Rule 유효성 판별
+"""
+@inout_apply.route('/rule/valid', methods=['POST'])
 def ruleValidate():
-
     ssctenant = Ssctenant.query.filter_by(event_url=request.host).first()
     tenant_id = ssctenant.tenant_id
+    print(request.form)
 
+    vsdate = request.form['sdate'] #방문시작 날짜
+    # vsdate = time.mktime(datetime.strptime(vsdate,'%Y-%m-%d').timetuple())
+
+    vedate = request.form['edate'] #방문종료 날짜
+    # vedate = time.mktime(datetime.strptime(vedate, '%Y-%m-%d').timetuple())
+
+    name = request.form['userName'] #사용자 이름
+    phone = request.form['userPhone'] #휴대폰 번호
+
+    ruleList = []
     lists = []
-    for row in db.session.query(Scrule).filter(Scrule.tenant_id == tenant_id).all():
-        scrule = {
-            "rule_name": row.rule_name,
-            "rule_type": row.rule_type,
-            "rule_duedate": row.rule_duedate,
-            "rule_desc": row.rule_desc
-        }
-        lists.append(scrule)
 
-    return jsonify({'msg': lists})
+    #1.현재 등록된 모든 Rule을 모두 가져온다.
+    for row in db.session.query(Scrule).all():
+        ruleList.append(row.rule_name)
+        dict[row.rule_name] = False
 
+
+    # 2.tenant에 등록된 RULE을 기준으로, name/phone/유효일자를 검색하는 로직, 규칙시작일(s_date) <=방문시작일(vsdate) / 규칙종료일(e_date) >=방문종료일(vedate)
+    for rule_name in ruleList :
+        for row in db.session.query(Vcvisituser)\
+                .filter(Vcvisituser.name == name, Vcvisituser.phone == phone,
+                        Vcvisituser.rule_name == rule_name,
+                        and_(Vcvisituser.s_date <= vsdate,Vcvisituser.e_date >= vedate)):
+           dict[row.rule_name] = True
+           lists.append(dict)
+
+
+    print(lists)
+
+
+    return jsonify({'msg': '와웅'})
 
 
 @inout_apply.route('/interview/search', methods=['POST'])
 def interviewSearch():
-
     if request.method == 'POST':
         interviewName = request.form['interviewName'];
         data = '{"name": "Book1", "ISBN": "12345", "author": [{"name": "autho1", "age": 30}, {"name": "autho2", "age": 25}]}'
         return jsonify({'file_name': 'output'});
-
 
 # print('============================')
 # print(applyform.stringfield.data)
