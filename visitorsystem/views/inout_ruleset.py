e1e4cc63e808980ec1a4a422768d9aa882746902
hojunghan@hanwha.com <hojunghan@hanwha.com>
2021-02-08 14:21:08 +0900

    code 수정

--- visitorsystem/views/inout_ruleset.py
+++ visitorsystem/views/inout_ruleset.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, current_app, jsonify, flash
+from flask_login import current_user
 
 from visitorsystem.models import db, Scrule, Ssctenant
 
@@ -7,19 +8,16 @@ inout_ruleset = Blueprint('inout_ruleset', __name__)
 
 @inout_ruleset.route('/', methods=['GET'])
 def index():
-    ssctenant = Ssctenant.query.filter_by(event_url=request.host).first()
-    tenant_id = ssctenant.tenant_id
-    scrules = db.session.query(Scrule).filter(Scrule.tenant_id == tenant_id).all()
+    tenant_id = current_user.ssctenant.id
+    scrules = db.session.query(Scrule).filter(Scrule.tenant_id == tenant_id, Scrule.use_yn == '1').all()
     return render_template(current_app.config['TEMPLATE_THEME'] + '/inout_ruleset/list.html'
                            , current_app=current_app
                            , scrules=scrules)
 
-
 @inout_ruleset.route('/save', methods=['POST'])
 def save():
     if request.method == 'POST':
-        ssctenant = Ssctenant.query.filter_by(event_url=request.host).first()
-        tenant_id = ssctenant.tenant_id
+        tenant_id = current_user.ssctenant.id
         rule_name = request.form['rule_name']
         result = db.session.query(Scrule).filter(Scrule.tenant_id == tenant_id, Scrule.rule_name == rule_name).all()
 
@@ -33,16 +31,14 @@ def save():
             scrule.rule_tlocation = request.form['rule_name']  # text정보 location
             db.session.add(scrule)
             db.session.commit()
-
-
         else:
             db.session.query(Scrule) \
                 .filter(Scrule.tenant_id == tenant_id, Scrule.rule_name == rule_name) \
-                .update({'rule_name': request.form['rule_name'],
-                         'rule_type': request.form['rule_type'],
-                         'rule_duedate': request.form['rule_duedate'],
-                         'rule_desc': request.form['rule_desc'],
-                         })
+                .update({
+                'rule_type': request.form['rule_type'],
+                'rule_duedate': request.form['rule_duedate'],
+                'rule_desc': request.form['rule_desc'],
+            })
             db.session.commit()
 
         return jsonify({'msg': '규칙이 성공적으로 저장되었습니다.'})
@@ -51,9 +47,8 @@ def save():
 @inout_ruleset.route('/edit', methods=['POST'])
 def edit():
     if request.method == 'POST':
-        ssctenant = Ssctenant.query.filter_by(event_url=request.host).first()
-        tenant_id = ssctenant.tenant_id
-        id = request.form['id']
+        tenant_id = current_user.ssctenant.id
+        id = int(request.form['id'])
 
         for row in db.session.query(Scrule).filter(Scrule.tenant_id == tenant_id, Scrule.id == id).all():
             scrule = {
@@ -62,17 +57,15 @@ def edit():
                 "rule_duedate": row.rule_duedate,
                 "rule_desc": row.rule_desc
             }
-
-        return jsonify({'msg': scrule})
+    return jsonify({'msg': scrule})
 
 
 @inout_ruleset.route('/delete', methods=['POST'])
 def delete():
     if request.method == 'POST':
-        ssctenant = Ssctenant.query.filter_by(event_url=request.host).first()
-        tenant_id = ssctenant.tenant_id
-        id = request.form['id']
+        tenant_id = current_user.ssctenant.id
+        id = int(request.form['id'])
         scrule = db.session.query(Scrule).filter(Scrule.tenant_id == tenant_id, Scrule.id == id).first()
-        db.session.delete(scrule)
+        scrule.use_yn = '0'
         db.session.commit()
         return jsonify({'msg': 'success'});
