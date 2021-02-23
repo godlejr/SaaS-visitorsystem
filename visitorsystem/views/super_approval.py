8332b7170a40ea13d6b7db996616db20d67ce41a
jungeunpark <jungeunpark@hanwha.com>
2021-02-23 23:02:06 +0900

    [2021.02.23] 감독부서승인 수정 (주석)

--- visitorsystem/views/super_approval.py
+++ visitorsystem/views/super_approval.py
@@ -43,7 +43,7 @@ def index(page):
                            query_string=request.query_string.decode('utf-8'))
 
 
-@super_approval.route('/search', methods=['GET', 'POST'])
+@super_approval.route('/search', methods=['POST'])
 def search():
     if request.method == 'POST':
         ssctenant = db.session.query(Ssctenant).filter_by(event_url=request.host).first()
@@ -117,7 +117,6 @@ def detail():
         }
         users.append(results)
 
-
     # 3. 사용자들의 Rule이 유효한지 판단 및 정보 저장
     bucketUrl = 'https://vms-tenants-rulefile-bucket-dev.s3.ap-northeast-2.amazonaws.com/'
 
@@ -177,10 +176,6 @@ def selectApplyListWithSearchCondition(searchCondition):
     offset = (pages * (page - 1)) if page != 1 else 0
 
     # 로그인한 사용자 권한에 따른 조회 데이터 변경 (계정당 권한 1개)
-    # AUTH_ADMIN = '9990' - 전체
-    # AUTH_VISIT_ADMIN = '3000' - 전체
-    # AUTH_VISITOR = '1000'  - 조회X
-    # AUTH_APPROVAL = '2000' - 로그인한 계정으로 요청온 것만
     userAuth = current_user.get_auth.code
 
     #일반 사용자는 조회는 Data 없도록 하기 위함. (없는 Data Select함으로써 type 형태만 갖춤)
